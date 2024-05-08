662a9b530b767c31cd7c7c16072b106423e9eb97
bonsthie <barnabe.bonnet@gmail.com>
2024-05-08 19:20:23 +0200

    ex00 cpp00 finish pog basic version #cleancode #yesthecode #hpc

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -5,52 +5,43 @@
 #if defined(__AVX2__)
 #include <immintrin.h>
 
-__m256i _mm256_isalpha_epi8(__m256i data)
+__m256i _mm256_islower_epi8(__m256i data)
 {
-	__m256i mask;
-	__m256i mask_a = _mm256_set1_epi8('a');
-	__m256i mask_z = _mm256_set1_epi8('z');
-
-	mask = _mm256_cmpgt_epi8(data, mask_a);
-	mask ^= _mm256_cmpgt_epi8(data, mask_z);
-	return (mask);
+    return _mm256_and_si256(
+	 _mm256_cmpgt_epi8(data, _mm256_set1_epi8('a' - 1)),
+     _mm256_cmpgt_epi8(_mm256_set1_epi8('z'), data));
 }
 
 __m256i _mm256_toupper_epi8(__m256i data)
 {
-	__m256i result;
-	__m256i mask = _mm256_isalpha_epi8(data);
-	__m256i upper_mask = _mm256_set1_epi8(0xDF);
+    __m256i mask = _mm256_islower_epi8(data);
+    __m256i upper_mask = _mm256_set1_epi8(0xDF);
+    __m256i lower_to_upper = _mm256_and_si256(data, upper_mask);
 
-	result = _mm256_or_si256(_mm256_and_si256(mask, _mm256_and_si256(data, upper_mask)),\
-			_mm256_andnot_si256(mask, data));
-	return (result);
+    return _mm256_or_si256(
+		_mm256_and_si256(mask, lower_to_upper),
+		_mm256_andnot_si256(mask, data));
 }
 
-void to_upper_avx2(std::string str)
+void to_upper_avx2(std::string& str)
 {
-	int len = str.size();
-	int i = 0;
+    size_t len = str.size();
+    size_t i = 0;
 
-	for (; i <= len - 32; i += 32)
-	{
-		__m256i data = _mm256_loadu_si256((__m256i*)(str.data() + i));
-		data = _mm256_toupper_epi8(data);
-		_mm256_storeu_si256((__m256i*)(str.data() + i), data);
-	}
-	for (; i < len; ++i)
-		str[i] = toupper(str[i]);
+    for (; i + 32 <= len; i += 32) {
+        __m256i data = _mm256_loadu_si256((__m256i*)(str.data() + i));
+        data = _mm256_toupper_epi8(data);
+        _mm256_storeu_si256((__m256i*)(str.data() + i), data);
+    }
+	std::transform(str.begin() + i, str.end(), str.begin() + i, (int(*)(int)) std::toupper);
 }
 #endif
 
-int megaphone(char **av)
+void megaphone(char **av)
 {
 	av++;
 	if (!*av)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (1);
-	}
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (; *av; av++)
 	{
 		std::string str = *av;
@@ -62,7 +53,6 @@ int megaphone(char **av)
 		std::cout << str;
 	}
 	std::cout << std::endl;
-	return (0);
 }
 
 int main(int ac, char **av)
